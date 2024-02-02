19c6825f922219bf0babd19d41f94bc3e2bdc30b
antoniodalterio <antoniodalterio2227@gmail.com>
2024-02-02 01:16:04 +0100

    feat: model

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Comics;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        $comics = Comics::all();
+        return view('welcome', compact('Comics'));
     }
 }
